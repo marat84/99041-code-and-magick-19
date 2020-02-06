12a2d21988ddb13683687a8e6306714afbe50715
Marat <marat84ay@gmail.com>
2020-02-06 12:08:23 +0500

    Fix: validity input name character

--- js/setup.js
+++ js/setup.js
@@ -7,6 +7,20 @@ var setupBlock = document.querySelector('.setup');
 var setupButtonOpen = document.querySelector('.setup-open');
 var setupButtonClose = document.querySelector('.setup-close');
 var setupInput = setupBlock.querySelector('.setup-user-name');
+var inputMinLength = +setupInput.getAttribute('minlength') || 2;
+var inputMaxLength = +setupInput.getAttribute('maxlength') || 25;
+
+setupInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+
+  if ((target.validity.tooShort || target.value.length < inputMinLength) && target.value.length !== 0) {
+    target.setCustomValidity('Имя персонажа не может содержать менее ' + inputMinLength + ' символов');
+  } else if ((target.validity.tooLong || target.value.length > inputMaxLength) && target.value.length !== 0) {
+    target.setCustomValidity('Максимальная длина имени персонажа — ' + inputMaxLength + ' символов');
+  } else {
+    target.setCustomValidity('');
+  }
+});
 
 var getRandomValue = function (values) {
   return values[Math.floor(Math.random() * values.length)];
