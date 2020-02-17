bccb5c7f084c22cde13f960ee91ef41dedff4d2a
Marat <marat84ay@gmail.com>
2020-02-17 08:31:55 +0500

    Fix: optimization setup.js code

--- js/setup.js
+++ js/setup.js
@@ -1,15 +1,6 @@
 'use strict';
 
 (function () {
-  var playerCoatClassName = window.utils.playerCoatClassName;
-  var playerEyesClassName = window.utils.playerEyesClassName;
-
-  var getRandomValue = window.utils.getRandomValue;
-  var getInputByName = window.utils.getInputByName;
-  var characterCoatColors = window.data.CHARACTER_COAT_COLORS;
-  var characterEyesColors = window.data.CHARACTER_EYES_COLORS;
-  var characterFireballColor = window.data.CHARACTER_FIREBALL_COLOR;
-
   var mainCharacter = window.utils.setupBlock.querySelector('.setup-player');
   var setupForm = window.utils.setupBlock.querySelector('.setup-wizard-form');
   var setupFormButton = setupForm.querySelector('.setup-submit');
@@ -17,23 +8,23 @@
 
   var filterClickHandler = function (evt) {
     var target = evt.target;
-    var randomCoatColor = getRandomValue(characterCoatColors);
-    var randomEyesColor = getRandomValue(characterEyesColors);
-    var randomFireballColor = getRandomValue(characterFireballColor);
+    var randomCoatColor = window.utils.getRandomValue(window.data.CHARACTER_COAT_COLOR);
+    var randomEyesColor = window.utils.getRandomValue(window.data.CHARACTER_EYES_COLORS);
+    var randomFireballColor = window.utils.getRandomValue(window.data.CHARACTER_FIREBALL_COLOR);
 
-    if (target && target.matches(playerCoatClassName)) {
+    if (target && target.matches(window.utils.playerCoatClassName)) {
       target.style.fill = randomCoatColor;
-      getInputByName('coat-color').value = randomCoatColor;
+      window.utils.getInputByName('coat-color').value = randomCoatColor;
     }
 
-    if (target && target.matches(playerEyesClassName)) {
+    if (target && target.matches(window.utils.playerEyesClassName)) {
       target.style.fill = randomEyesColor;
-      getInputByName('eyes-color').value = randomEyesColor;
+      window.utils.getInputByName('eyes-color').value = randomEyesColor;
     }
 
     if (target && target.matches('.setup-fireball')) {
       target.parentElement.style.background = randomFireballColor;
-      getInputByName('fireball-color').value = randomFireballColor;
+      window.utils.getInputByName('fireball-color').value = randomFireballColor;
     }
   };
 
