2d7b12f62654f9e0d2d57dd5dcd0cac163f290a6
Marat <marat84ay@gmail.com>
2020-02-17 08:33:39 +0500

    Fix: optimization generate-character.js code

--- js/generate-character.js
+++ js/generate-character.js
@@ -4,8 +4,6 @@
   var CHARACTER_COUNT = 4;
 
   var setupBlock = window.utils.setupBlock;
-  var playerCoatClassName = window.utils.playerCoatClassName;
-  var playerEyesClassName = window.utils.playerEyesClassName;
   var characterTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
   var characterSimilarList = setupBlock.querySelector('.setup-similar-list');
 
@@ -13,8 +11,8 @@
   var renderCharacter = function (character) {
     var characterClone = characterTemplate.cloneNode(true);
 
-    characterClone.querySelector(playerCoatClassName).style.fill = character.colorCoat;
-    characterClone.querySelector(playerEyesClassName).style.fill = character.colorEyes;
+    characterClone.querySelector(window.utils.playerCoatClassName).style.fill = character.colorCoat;
+    characterClone.querySelector(window.utils.playerEyesClassName).style.fill = character.colorEyes;
     characterClone.querySelector('.setup-similar-label').textContent = character.name;
 
     return characterClone;
